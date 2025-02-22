45ee9c92b62a4b0aed13ca717f097817f3c1df9c
jjyanangomez <juanyasa_04@hotmail.com>
2025-02-22 01:14:25 -0500

    seguridad y consumo

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-    user: 'jorgesarmiento',
-    host: 'localhost',
-    database: 'licorera_db',
-    password: '',
-    port: 5432,
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port,
 });
 
 module.exports = pool;
